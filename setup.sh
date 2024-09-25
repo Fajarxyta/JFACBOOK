b61677b576df60f5ff0dfd00fd8bac3272c3d171
Fajarky <fajarstok15@gmail.com>
2024-09-25 20:07:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,37 @@
-njir
+#!/bin/bash
+set -e
+
+echo "Memeriksa apakah pip sudah terinstal..."
+if ! command -v pip &> /dev/null
+then
+    echo "pip tidak ditemukan, silakan instal pip terlebih dahulu."
+    exit 1
+else
+    echo "pip terdeteksi. Melanjutkan..."
+fi
+
+echo "Memperbarui pip ke versi terbaru..."
+pip install --upgrade pip
+
+packages=(
+    pycryptodome
+    cryptography
+    requests
+    rich
+    futures
+    pycryptodomex
+    PyNaCl
+)
+
+echo "Memulai proses instalasi paket..."
+for package in "${packages[@]}"
+do
+    if python -c "import $package" &> /dev/null; then
+        echo "Paket '$package' sudah terinstal, melewati..."
+    else
+        echo "Menginstal '$package'..."
+        pip install $package
+    fi
+done
+
+echo "Semua paket telah terinstal atau sudah ada!"
